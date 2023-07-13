ae7facb2c5c714ca8f2bf85efd2ecabb3dce1b20
Ítalo Penha <italopenha77@outlook.com>
2023-07-13 18:57:58 -0300

    Finalizando o projeto final

--- ProjetoYouTube/src/classes/Video.java
+++ ProjetoYouTube/src/classes/Video.java
@@ -30,7 +30,9 @@ public class Video implements AcoesVideo {
 	}
 
 	public void setAvaliacao(int avaliacao) {
-		this.avaliacao = avaliacao;
+		int nova = 0;
+		nova = (int) (this.avaliacao + avaliacao) / this.views;
+		this.avaliacao = nova;
 	}
 
 	public int getViews() {
--- ProjetoYouTube/src/principal/Principal.java
+++ ProjetoYouTube/src/principal/Principal.java
@@ -2,6 +2,7 @@ package principal;
 
 import classes.Gafanhoto;
 import classes.Video;
+import classes.Visualizacao;
 
 public class Principal {
 
@@ -15,7 +16,22 @@ public class Principal {
 		g[0] = new Gafanhoto("Jubileu", 22, "M", "juba");
 		g[1] = new Gafanhoto("Creuza", 12, "F", "creuzita");
 		
-		System.out.println(v[0].toString());
-		System.out.println(g[0].toString());
+		Visualizacao vis[] = new Visualizacao[5];
+		vis[0] = new Visualizacao(g[0], v[2]);
+		vis[0].avaliar();
+		System.out.println(vis[0].toString());
+		
+		vis[1] = new Visualizacao(g[0], v[1]);
+		vis[1].avaliar(87.0f);
+		System.out.println(vis[1].toString());
+		
+		
+//		System.out.println("V͍DEOS\n---------------------");
+//		System.out.println(v[0].toString());
+//		System.out.println(v[1].toString());
+//		System.out.println(v[2].toString());
+//		System.out.println("\nGAFANHOTOS\n---------------");
+//		System.out.println(g[0].toString());
+//		System.out.println(g[1].toString());
 	}
 }
