98dfa97a06dc7e46a9c4709d6427eabe73edae98
Ítalo Penha <italopenha77@outlook.com>
2023-07-13 17:16:37 -0300

    Adicionando exercício de polimorfismo

---
+++ Aula13/src/principal/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+import classes.Cachorro;
+import classes.Lobo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Cachorro c = new Cachorro();
+		c.reagir(2, 12.5f);
+		c.reagir(17, 4.5f);
+	}
+}
